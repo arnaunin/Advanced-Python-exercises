897ec5f8ab0ca9a313d88175f96cc77f71a5aaa6
Arnau Nin <ninbsnss92@gmail.com>
2025-04-04 19:15:08 +0200

    Log in verification

---
+++ Decorators/Verify-Log-In/verify-log-in.py
@@ -0,0 +1,22 @@
+import json
+
+with open('users.json','r') as json_file:
+    users = json.load(json_file)
+
+def verify_log_in(func):
+    def wrapper(username, password):
+        if username in users and password == users[username]['password']:
+            print(f"Successful login for the user '{username}'")
+            return func(users[username])
+        else:
+            print("Login failed. Verify your username and password")
+    
+    return wrapper
+
+@verify_log_in
+def user_information(user):
+    print("User personal information:")
+    print(f"Full name: {user['full_name']}")
+    print(f"Email adress: {user['email']}")
+
+user_information('user1', 'password123')
