e55335d8175b54a151679a59ec6d26cd050bee9d
Arnau Nin <ninbsnss92@gmail.com>
2025-04-04 19:38:54 +0200

    Name change

--- Decorators/Verify-Log-In/verify-log-in.py
+++ Decorators/Verify-Log-In/verify-log-in.py
@@ -6,10 +6,10 @@ with open('users.json','r') as json_file:
 def verify_log_in(func):
     def wrapper(username, password):
         if username in users and password == users[username]['password']:
-            print(f"Successful login for the user '{username}'.")
+            print(f"Successful login for the user '{username}'")
             return func(users[username])
         else:
-            print("Login failed. Verify your username and password.")
+            print("Login failed. Verify your username and password")
     
     return wrapper
 
